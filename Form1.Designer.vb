c801359e57f3e4b1eb81b0367c3fc317c377a087
USER <USER@ZULBIASA02>
2022-04-05 11:35:05 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.btnSum = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(44, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(129, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter a number"
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(207, 48)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(85, 30)
+        Me.txtInput.TabIndex = 1
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.lblOutput.ForeColor = System.Drawing.Color.Yellow
+        Me.lblOutput.Location = New System.Drawing.Point(44, 104)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(248, 32)
+        Me.lblOutput.TabIndex = 2
+        '
+        'btnSum
+        '
+        Me.btnSum.Location = New System.Drawing.Point(114, 150)
+        Me.btnSum.Name = "btnSum"
+        Me.btnSum.Size = New System.Drawing.Size(108, 32)
+        Me.btnSum.TabIndex = 3
+        Me.btnSum.Text = "Summation"
+        Me.btnSum.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 23.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(336, 206)
+        Me.Controls.Add(Me.btnSum)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "  "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents btnSum As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub btnSum_Click(sender As Object, e As EventArgs) Handles btnSum.Click
+        Dim intSum As Integer
+
+        If (Val(txtInput.Text) > 0) Then
+            intSum = 0
+            For x = 1 To Val(txtInput.Text)
+                intSum = intSum + x
+            Next x
+            lblOutput.Text = "Summation of " & txtInput.Text & " is" & Str(intSum) & "."
+        Else
+            lblOutput.Text = "Enter a positive integer only."
+        End If
+    End Sub
+End Class
